86453dc6e6bc8872389b3affa799b28a3de65fda
celso-rodrigo <celsorodrigoma@gmail.com>
2023-03-14 13:11:40 -0300

    Concluído 11° requisito.

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -1,3 +1,35 @@
+from sys import stderr
+
+
 # Requisitos 11 e 12
 def analyzer_menu():
-    """Seu código deve vir aqui"""
+    """
+    Responsável pelo menu do nosso programa
+    """
+
+    print(
+        "Selecione uma das opções a seguir:\n"
+        " 0 - Popular o banco com notícias;\n"
+        " 1 - Buscar notícias por título;\n"
+        " 2 - Buscar notícias por data;\n"
+        " 3 - Buscar notícias por categoria;\n"
+        " 4 - Listar top 5 categorias;\n"
+        " 5 - Sair."
+    )
+
+    responses = [
+        "Digite quantas notícias serão buscadas:",
+        "Digite o título:",
+        "Digite a data no formato aaaa-mm-dd:",
+        "Digite a categoria:",
+        "Opção inválida",
+    ]
+
+    try:
+        user_input = int(input())
+        if user_input in range(4):
+            print(responses[user_input])
+        elif user_input not in range(6):
+            print("Opção inválida", file=stderr)
+    except ValueError:
+        return print("Opção inválida", file=stderr)
